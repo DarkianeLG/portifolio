9855ea56460e574af81bd24df0aaf55209705d18
Darkiane Lima <darkianelima13@gmail.com>
2024-04-12 21:48:16 -0300

    Fazendo o Media Queries e criando o javaCript

---
+++ script.js
@@ -0,0 +1,17 @@
+let botãoMostrarProjeto = document.querySelector('.btn-mostrar-projetos');
+let projetosInativos = document.querySelectorAll('projeto:not(.ativo)');
+
+botãoMostrarProjeto.addEventListener('click', () => {
+    mostrarProjetosInativos();
+    escoderBotão();
+});
+
+function mostrarProjetosInativos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+
+}
+function escoderBotão() {
+    botãoMostrarProjeto.classList.add('remover');
+}
